c02d2b8335258e82c150f88ba467fb1a0c50e9bd
Daniel Hawton <daniel@hawton.com>
2018-05-30 23:46:10 -0800

    need to rebuild directories apparently, and tweak members to show vatusa10

--- build.sh
+++ build.sh
@@ -7,3 +7,6 @@ cat /run/secrets/*.env >> /www/.env
 chown application:application /www/.env
 
 echo "*    *    *     *     *    cd /www && php artisan schedule:run" > /etc/crontabs/application
+
+mkdir /www/storage/framework/views
+chown application:application /www/storage/framework/views
