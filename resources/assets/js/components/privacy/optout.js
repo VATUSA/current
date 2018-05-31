e8062c284057b3ee0251427309410c4ad9880fb6
Daniel Hawton <daniel@hawton.com>
2018-05-30 23:31:25 -0800

    Change optout directions per Mark

--- resources/assets/js/components/privacy/optout.js
+++ resources/assets/js/components/privacy/optout.js
@@ -1,11 +1,9 @@
 const optout = `
 <h1>Opt Out</h1>
-<h4 style="font-color: red"><u>NOTE:</u> This section of the policy goes into effect on May 25, 2018 and may not be available prior to that date.</h4>
 <p>Given the nature of our services, it is not possible to opt out of data collection and use our services.  But if you
 desire to opt out and no longer desire to use our services, we will purge all information we have collected upon written request.</p>
 <p>The first step is to deactivate and request VATSIM to purge your data.  Please head to <a href="https://membership.vatsim.net/">https://membership.vatsim.net/</a> to do so.</p>
-<p>After VATSIM has purged your data, please send a written request to VATUSA's Data Services Manager by emailing vatusa6[at]vatusa.net.  It is a manual process, so please 
-allow up to 14 days for information to be purged.  You will receive an email response once the data has been purged.</p>
+<p>VATSIM, after processing your opt out, will forward the request to VATUSA for processing.  Users may also contact VATUSA 10 at vatusa10@vatusa.net for guidance.</p>
 <p><b>Note</b> VATUSA cannot guarantee that information collected by parties outside of VATUSA will be purged in the process.</p>
 `;
 
