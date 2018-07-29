6ddf2af9ffaea0fcec983bce0790ed31330cb098
Blake Nahin <vatusa12@vatusa.net>
2018-07-29 00:19:54 -0700

    Change form implementation

--- resources/views/mgt/ace.blade.php
+++ resources/views/mgt/ace.blade.php
@@ -11,29 +11,32 @@
             <div class="panel-body">
                 <div class="row">
                     <div class="col-md-12">
-                    {!! Form::open(array('url' => '/mgt/ace')) !!}
-                        Add Controller: <input type="text" name="cid"> <button type="submit" class="btn btn-info">Add</button>
-                    {!! Form::close() !!}
-                    <table class="table table-striped">
-                        @foreach ($roles as $ace)
-                            <tr>
-                                <td width="10%">{{$ace->cid}}</td>
-                                <td width="80%">{{$ace->user()->first()->fullname()}}</td>
-                                <td><button class="btn btn-danger" OnClick="aceDelete({{$ace->cid}})">Delete</button></td>
-                            </tr>
-                        @endforeach
-                    </table>
+                        <form class="form-inline" action="{{ secure_url('/mgt/ace') }}" style="margin-bottom:5px;">
+                            <label for="cid">Add Controller:</label> <input type="text" name="cid" class="form-control">
+                            <button type="submit" class="btn btn-info">Add</button>
+                        </form>
+                        <table class="table table-striped">
+                            @foreach ($roles as $ace)
+                                <tr>
+                                    <td width="10%">{{$ace->cid}}</td>
+                                    <td width="80%">{{$ace->user()->first()->fullname()}}</td>
+                                    <td>
+                                        <button class="btn btn-danger" OnClick="aceDelete({{$ace->cid}})">Delete
+                                        </button>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </table>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-<script type="text/javascript">
-    function aceDelete(cid)
-    {
-        bootbox.confirm("Are you sure you want to delete " + cid + " from the ACE team?", function(result) {
-            if (result === true) window.location="/mgt/ace/delete/"+cid;
-        });
-    }
-</script>
+    <script type="text/javascript">
+      function aceDelete (cid) {
+        bootbox.confirm('Are you sure you want to delete ' + cid + ' from the ACE team?', function (result) {
+          if (result === true) window.location = '/mgt/ace/delete/' + cid
+        })
+      }
+    </script>
 @stop
