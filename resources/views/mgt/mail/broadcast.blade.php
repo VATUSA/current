6c56e67e484f11d7327e406cb3cec9627204a82b
Blake Nahin <bnahin@live.com>
2018-12-16 21:49:16 -0800

    Fix non-submitted broadcast form

--- resources/views/mgt/mail/broadcast.blade.php
+++ resources/views/mgt/mail/broadcast.blade.php
@@ -2,8 +2,10 @@
 
 @section('scripts')
     <script>
-      $('#send-broadcast').click(function () {
+      $('#send-broadcast').click(function (e) {
+        e.preventDefault()
         $(this).attr('disabled', true).html('<i class="spinner-icon fa fa-spinner fa-spin"></i>')
+        $(this.form).submit()
       })
     </script>
 @endsection
@@ -33,24 +35,33 @@
                   method="POST">
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
                 <div class="form-group">
-                    <label class="col-sm-2 control-label">To:</label>
+                    <label class="col-sm-2 control-label" for="recipients">To:</label>
                     <div class="col-sm-10">
                         @if(empty($cid))
-                            <select class="form-control" name="recipients">
+                            <select class="form-control" name="recipients" id="recipients">
                                 @if(\App\Classes\RoleHelper::isVATUSAStaff())
-                                    <option value="STAFF">All STAFF</option>
-                                    <option value="ALL">All VATUSA Controllers</option>
-                                    <option value="SRSTAFF">All ATMs DATMs & TAs</option>
-                                    <option value="DRCTR">All ATMs and DATMs</option>
-                                    <option value="WM">All WMs</option>
-                                    <option value="VATUSA">All VATUSA STAFF</option>
-                                    <option value="INS">All INSTRUCTIONAL STAFF</option>
-                                    <option value="ACE">All ACE Team Members</option>
+                                    <optgroup label="All Facilities">
+                                        <option value="ALL">All VATUSA Controllers</option>
+                                        <option value="STAFF">All Facility Staff</option>
+                                        <option value="SRSTAFF">All Senior Staff (ATM/DATM/TA)</option>
+                                        <option value="DRCTR">All ATMs and DATMs</option>
+                                        <option value="WM">All WMs</option>
+                                        <option value="INS">All Instructors (I1+)</option>
+                                    </optgroup>
+                                    <optgroup label="VATUSA">
+                                        <option value="VATUSA">All VATUSA Staff (Includes ACE)</option>
+                                        <option value="ACE">All ACE Team Members</option>
+                                    </optgroup>
+                                    <optgroup label="Facilities">
+                                        @foreach(\App\Facility::where('active', 1)->orderby('name', 'ASC')->get() as $f)
+                                            <option value="{{$f->id}}">{{$f->name}}</option>
+                                        @endforeach
+                                    </optgroup>
+                                @else
+                                    <option value="{{\Auth::user()->facility}}"
+                                            selected>{{\App\Classes\Helper::facShtLng(\Auth::user()->facility)}}
+                                    </option>
                                 @endif
-                                @foreach(\App\Facility::where('active', 1)->orderby('name', 'ASC')->get() as $f)
-                                    <option value="{{$f->id}}"
-                                            @if ($f->id == \Auth::user()->facility) selected="true" @endif>{{$f->name}}</option>
-                                @endforeach
                             </select>
                         @else
                             <p class="form-control-static">{{\App\Classes\Helper::nameFromCID($cid)}} ({{$cid}})</p>
