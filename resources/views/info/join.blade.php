7730cef3ad346ffdb71c3ab40cd411ebe2619437
Blake Nahin <vatusa12@vatusa.net>
2018-12-23 23:24:17 -0600

    Update Join Us page

--- resources/views/info/join.blade.php
+++ resources/views/info/join.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-md-12">
                 <h3>Join as ATC</h3>
-                <p>First and foremost, thank you for taking the time to consider joining the Virtual Air Traffic
+                <p>Thank you for taking the time to consider joining the Virtual Air Traffic
                     Simulation Network's
                     USA Division (VATUSA). This network represents the single, most complex airspace in the world
                     featuring 2015's world's
@@ -13,7 +13,11 @@
                     on average, over 2,000,000
                     passengers traverse across the US airspace on Air Transport (followed by China at just over 1
                     million, and the UK at 350,000).</p>
-                <b>How do I become a controller in VATUSA?</b>
+                <div class="alert alert-warning"><i class="fa fa-info-circle"></i> <b>INFO:</b> It may take up to 24 hours for new, reactived or
+                    transferred members to be processed and sent to VATUSA through server synchronization. Please allow
+                    24 hours to elapse before submitting support tickets.
+                </div>
+                <b>How do I become a controller in VATUSA (or rejoin after 6+ months of inactivity)?</b>
                 <ol>
                     <li>Join VATSIM at <a href="http://www.vatsim.net">www.vatsim.net</a></li>
                     <li>Join the North America region - United States Division. If you are a VATSIM member, but not yet
@@ -24,35 +28,65 @@
                                 VATCAR, please email vatusa1@vatusa.net to continue.
                             </li>
                             <li>If you are a member of a different region, head over to <a
-                                        href="https://cert.vatsim.net/vatsimnet/regch.php">Region Change Request</a> and
+                                    href="https://cert.vatsim.net/vatsimnet/regch.php">Region Change Request</a> and
                                 fill out the form.
                                 This process is not immediate and can take 4-7 days to complete.
                             </li>
                         </ul>
                     </li>
                     <li>
-                        After you join the VATUSA division, you must complete the "Basic/S1 Exam."  Click on the "Assign Basic ATC Exam" under "My VATUSA" to be assigned the exam.  If it's already assigned, that link will not appear.  Go to "Exam Center" under "My VATUSA".
-                        <b>Before you complete the exam, you must complete the "<u>Block 1</u> - Basic ATC/S1" Computer Based Training (CBT) course.  This can be found at <a href="/cbt">https://www.vatusa.net/cbt</a></b>.  <u>ALL</u> items under Block 1 must be checked complete before you will be permitted to start the exam.
+                        After you join the VATUSA division, you must complete the <strong>"<u>Block 1</u> - Basic
+                            ATC/S1"</strong> Computer Based Training (CBT) course. This can be found at <a href="/cbt">https://www.vatusa.net/cbt</a></b>
+                        . <u>ALL</u> items under Block 1 must be checked complete before you will be permitted to start
+                        the Basic ATC Exam.
+                    </li>
+                    <li> Pass the "Basic/S1 Exam." Click on the "Assign Basic ATC Exam" under "My VATUSA" to be assigned
+                        the exam. If it's already assigned, that link will not appear, and can be taken from the "Exam
+                        Center" under "My VATUSA".
                     </li>
 
-                    <li>After completing the initial exam, you will be able to join your first facility. VATUSA is
+                    <li>After completing the initial exam, you will be able to join a facility. VATUSA is
                         made up of 21 ARTCCs and 1 CCF
                         [realworld FAA includes 2 CENRAPs, Guam which is under jurisdiction of Honolulu Combined Control
                         Facility and San Juan CENRAP which is part of VATCAR on VATSIM].
                         To join your first facility, go to "My VATUSA" in the Navigation Bar, then select "Join
-                        Facility".
+                        Facility". <br>
+                        <strong>If you are a returning member, you must request a transfer into your ARTCC of
+                            choice.</strong>
+                        <br>
+                        Once you have successfully joined a facility, you will receive a welcome email upon facility
+                        selection. Initial selections are immediate; those requesting a transfer will receive the
+                        welcome email once the transfer request has been accepted. This should include instructions on
+                        what to do next. You may receive subsequent follow ups once they have added you to their
+                        facility website. If you do not hear anything within a few days, please open a ticket with your
+                        facility.
                     </li>
 
-                    <li><b>Before you will obtain your S1 rating, you must complete training and an over-the-shoulder examination at your initial facility.  After joining, the process will be explained to you further.</b></li>
+                    <li><b>Before you obtain your S1 rating or return from inactivity, you must complete training and an
+                            over-the-shoulder examination at your initial facility. After joining, the process will be
+                            explained to you further.</b></li>
                 </ol>
-                <div class="alert alert-warning"><b>NOTE:</b> It may take up to 48 hours for new, reactived or transferred members to be processed and sent to VATUSA through server synchronization.  Please allow 48 hours to elapse before submitting support tickets.</div>
+                <strong>Important Notes</strong>
+                <ul>
+                    <li>If your VATSIM membership has lapsed (become inactive), you will need to reactivate your VATSIM membership <em>before</em>
+                        you can reactivate your membership with VATUSA. You can do so <a
+                            href="https://cert.vatsim.net/vatsimnet/statcheck.html">here</a>.
+                    </li>
+                    <li>If you are returning from an absence of 6 months or less, you may have been removed from your home ARTCC. Request a transfer to initiate the rejoin process.</li>
+                    <li><strong>It can take up to 24 hours for new, reactivated, or transfers to be processed and sent
+                            to VATUSA through server synchronization.</strong> Synchronizations occur at midnight and
+                        noon US Central time. As such, please do not submit membership tickets within 24 hours of
+                        joining VATSIM or reactivating VATSIM membership.
+                    </li>
+                </ul>
             </div>
         </div>
         <div class="row">
             <div class="col-md-12">
                 <h3>Join as Pilot</h3>
-                <p>To join a pilot, all you need is a <a href="https://www.vatsim.net">VATSIM account</a>.  For more information,
-                see VATSIM's <a href="https://www.vatsim.net/pilots/getting-started">Getting Started</a> page.</p>
+                <p>To join a pilot, all you need is a <a href="https://www.vatsim.net">VATSIM account</a>. For more
+                    information,
+                    see VATSIM's <a href="https://www.vatsim.net/pilots/getting-started">Getting Started</a> page.</p>
             </div>
         </div>
     </div>
