80c0c0cc3ab3965448e56d02221b08b99a1b571a
Blake Nahin <bnahin@live.com>
2018-12-27 23:40:50 -0800

    Add Sentry error tracking

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -9,69 +9,81 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Auth\AuthenticationException;
 
-class Handler extends ExceptionHandler {
+class Handler extends ExceptionHandler
+{
 
-	/**
-	 * A list of the exception types that should not be reported.
-	 *
-	 * @var array
-	 */
+    /**
+     * A list of the exception types that should not be reported.
+     *
+     * @var array
+     */
     protected $dontReport = [
         HttpException::class,
         ModelNotFoundException::class,
         ValidationException::class,
     ];
 
-	/**
-	 * Report or log an exception.
-	 *
-	 * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
-	 *
-	 * @param  \Exception  $e
-	 * @return void
-	 */
-	public function report(Exception $e)
-	{
-		return parent::report($e);
-	}
+    /**
+     * Report or log an exception.
+     *
+     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+     *
+     * @param  \Exception $e
+     *
+     * @return void
+     * @throws \Exception
+     */
+    public function report(Exception $e)
+    {
+        if (app()->bound('sentry') && $this->shouldReport($e)) {
+            app('sentry')->captureException($e);
+        }
 
-	/**
-	 * Render an exception into an HTTP response.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Exception  $e
-	 * @return \Illuminate\Http\Response
-	 */
-	public function render($request, Exception $e)
-	{
+        parent::report($e);
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Exception               $e
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function render($request, Exception $e)
+    {
         if ($request->server->get("SERVER_NAME") == "api.vatusa.net") {
             if ($e instanceOf NotFoundHttpException) {
                 return response()->json(["status" => "error", "msg" => "Request not known"]);
-            }
-            else {
+            } else {
                 return response()->json(["status" => "error", "msg" => "Unknown error"]);
             }
         }
 
-		if ($e instanceof MethodNotAllowedHttpException) {
-			return redirect('/405');
-		}
+        if ($e instanceof MethodNotAllowedHttpException) {
+            return redirect('/405');
+        }
 
-		if ($this->isHttpException($e))
-		{
-			return $this->renderHttpException($e);
-		}
-		else
-		{
-			return parent::render($request, $e);
-		}
-	}
+        if ($this->isHttpException($e)) {
+            return $this->renderHttpException($e);
+        } else {
+            // Convert all non-http exceptions to a proper 500 http exception
+            // if we don't do this exceptions are shown as a default template
+            // instead of our own view in resources/views/errors/500.blade.php
+            if ($this->shouldReport($e) && !config('app.debug')) {
+                $e = new HttpException(500, 'Whoops! An error occurred.');
+            }
+
+            return parent::render($request, $e);
+        }
+    }
 
     /**
      * Convert an authentication exception into an unauthenticated response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @param  \Illuminate\Http\Request                 $request
+     * @param  \Illuminate\Auth\AuthenticationException $exception
+     *
      * @return \Illuminate\Http\Response
      */
     protected function unauthenticated($request, AuthenticationException $exception)
