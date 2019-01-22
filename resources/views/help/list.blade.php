52c13402d92ac13258fc0707c00176066293699b
Blake Nahin <vatusa6@vatusa.net>
2019-01-21 21:33:34 -0800

    Support Center titles

--- resources/views/help/list.blade.php
+++ resources/views/help/list.blade.php
@@ -1,4 +1,5 @@
 @extends('layout')
+@section('title', 'Support Center')
 
 @section('content')
     <div class="container">
--- resources/views/help/listclosed.blade.php
+++ resources/views/help/listclosed.blade.php
@@ -1,4 +1,5 @@
 @extends('layout')
+@section('title', 'Closed Tickets')
 
 @section('content')
     <div class="container">
