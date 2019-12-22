1399c3f9b6037396521d9f4ab2e56723f2a5a77a
Brayden Manzella <bradym55@gmail.com>
2019-12-22 12:03:59 -0500

    Add temporary link to exit survey

--- resources/views/emails/user/removed.blade.php
+++ resources/views/emails/user/removed.blade.php
@@ -5,6 +5,9 @@ This email is to inform you that you have been removed from the {{$facility}} fa
 The following is the reason for your removal:<br><br>
 {{$msg}}
 <br><br>
+In an effor to better serve our controllers, we would appreciate if you could take a few minutes to complete an anonymous survey regarding your experience at {{$facility}}.
+The survey link can be found here: https://form.jotform.com/80467151384155
+<br><br>
 If you have any questions regarding your removal, please contact {{$facid}}-atm@vatusa.net and/or vatusa{{$region}}@vatusa.net.<br>
 <br>
 Regards,<br>
