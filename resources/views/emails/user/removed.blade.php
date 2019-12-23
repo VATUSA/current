039ae1028fea19761fc62bdd4f8e8959fb956894
Blake Nahin <bnahin@live.com>
2019-12-22 22:56:05 -0700

    OBS Inactivity survey link

--- resources/views/emails/user/removed.blade.php
+++ resources/views/emails/user/removed.blade.php
@@ -3,9 +3,18 @@ Dear {{$name}},<br><br>
 This email is to inform you that you have been removed from the {{$facility}} facility by {{$by}}.<br>
 <br>
 The following is the reason for your removal:<br><br>
-{{$msg}}
+<em>{{$msg}}</em>
+
+@if($obsInactive)
+    <br><br>
+    Thank you for taking a step towards becoming a controller on VATSIM. To improve our onboarding process, please take a short survey about your experience. Your responses will remain anonymous.
+    <br>
+    <a href="https://form.jotform.com/80467151384155">https://form.jotform.com/80467151384155</a>
+@endif
+
 <br><br>
-If you have any questions regarding your removal, please contact {{$facid}}-atm@vatusa.net and/or vatusa{{$region}}@vatusa.net.<br>
+If you have any questions regarding your removal, please contact {{$facid}}-atm@vatusa.net and/or vatusa{{$region}}@vatusa.net.
+<br>
 <br>
 Regards,<br>
 VATUSA Data Services
