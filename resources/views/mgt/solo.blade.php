7c3baefbfc732748042849f46d956bbe4570eb3b
Blake Nahin <vatusa12@vatusa.net>
2019-12-25 18:22:33 -0800

    Use API for solo endorsements

--- resources/views/mgt/solo.blade.php
+++ resources/views/mgt/solo.blade.php
@@ -1,4 +1,8 @@
 @extends('layout')
+@section('title', 'Manage Solo Endorsements')
+@push('scripts')
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+@endpush
 @section('content')
     <div class="container">
         <div class="row">
@@ -21,19 +25,24 @@
                             <td>{{$cert->position}}</td>
                             <td>{{$cert->expires}}</td>
                             <td>
-                                <button type="button" class="btn btn-danger" onClick="deleteCert({{$cert->id}})"><i
-                                            class="fa fa-times"></i></button>
+                                <button type="button" class="btn btn-danger delete-solo" data-id="{{ $cert->id }}"><i
+                                        class="fa fa-times"></i></button>
                             </td>
                         </tr>
                     @endforeach
-                    <form action="/mgt/solo" method="post">
+                    <form action="/mgt/solo" id="add-solo-form">
                         <input type="hidden" name="_token" value="{{csrf_token()}}">
                         <tr>
-                            <td colspan="2"><input type="text" name="cid" placeholder="CID" class="form-control" style="width:110px;"></td>
-                            <td><input type="text" name="position" placeholder="Position" class="form-control" style="width:150px"></td>
-                            <td><input type="date" name="expiration" placeholder="Expiration (YYYY-MM-DD)" class="form-control" style="padding:0; width:150px;"></td>
+                            <td colspan="2"><input type="text" name="cid" placeholder="CID" class="form-control"
+                                                   style="width:110px;"></td>
+                            <td><input type="text" name="position" placeholder="Position" class="form-control"
+                                       style="width:150px"></td>
+                            <td><input type="date" name="expDate" placeholder="Expiration (YYYY-MM-DD)"
+                                       class="form-control" style="padding:0; width:150px;"></td>
                             <td>
-                                <button type="submit" class="btn btn-success"><i class="fa fa-check"></i> Add</button>
+                                <button class="btn btn-success" id="add-solo"><i class="fa fa-check"></i>
+                                    Add
+                                </button>
                             </td>
                         </tr>
                     </form>
@@ -43,13 +52,36 @@
         </div>
     </div>
     <script type="text/javascript">
-        function deleteCert(id) {
-            $.ajax({
-                url: '/mgt/solo/' + id,
-                type: 'DELETE'
-            }).success(function () {
-                location.reload(true);
-            });
-        }
+      $(function () {
+        $('.delete-solo').click(function () {
+          let btn = $(this),
+              id  = btn.data('id')
+          btn.prop('disabled', true)
+          $.ajax({
+            method: 'DELETE',
+            url   : $.apiUrl() + '/v2/solo/',
+            data  : {id: id}
+          }).done(function () {
+            swal('Success!', 'Solo endorsement deleted.', 'success').then(() => { location.reload(true) })
+          }).error(function () {
+            swal('Error!', 'Unable to delete solo endorsement. Try again later.', 'error')
+            btn.prop('disabled', false)
+          })
+        })
+        $('#add-solo').click(function (e) {
+          e.preventDefault()
+          let btn  = $(this),
+              form = $('#add-solo-form')
+          btn.prop('disabled', true)
+          $.ajax({method: 'POST', url: $.apiUrl() + '/v2/solo', data: form.serialize()})
+            .done(function (result) {
+              swal('Success!', 'Solo endorsement has been added.', 'success').then(() => { location.reload() })
+            })
+            .error(function (result) {
+              btn.prop('disabled', false)
+              swal('Error!', 'Unable to add solo endorsement. ' + result.responseJSON.msg, 'error')
+            })
+        })
+      })
     </script>
 @endsection
