64ccde282e4fa81a28fe97698e3234809b85d934
Blake Nahin <vatusa12@vatusa.net>
2019-12-30 03:59:59 -0800

    Edit initial join email

--- resources/views/emails/user/join.blade.php
+++ resources/views/emails/user/join.blade.php
@@ -1,19 +1,25 @@
 <p>Welcome to the VATUSA Division.</p>
 
-<p>If you desire to be an Air Traffic Controller within the VATUSA division, please continue reading this email.  If you
+<p>If you desire to be an Air Traffic Controller within the VATUSA division, please continue reading this email. If you
     desire only to fly, you do not need to read this email and can discard it.</p>
 
-<p>Firstly, thank you for taking the time to join the division and we look forward to working the scopes with you.  Firstly,
-to continue the signup process you need to complete the Basic/S1 Computer Based Training (CBT) course located under "My VATUSA" and "Computer Based Training (CBT).
-    Then you will need to complete the Basic ATC Exam.  When you are ready to take this exam, please click "My VATUSA" then "Assign Basic ATC Exam".  Take your time with this exam, and remember it is open
-    book.  If you do not pass, you will need to wait 3 days to retake so take your time.</p>
+<p>Firstly, thank you for taking the time to join the division and we look forward to working the scopes with you.
+    Firstly, to continue the signup process you need to complete Blocks 1 and 2 (Basic/S1) of the Computer Based
+    Training (CBT) courses located under "My VATUSA" and "Computer Based Training" (CBT).
+    Then you will need to complete the Basic ATC Exam. <strong>The two CBT blocks must be completed before you can take
+        the exam.</strong> When you are ready to take this exam, please click "My VATUSA" then "Assign Basic ATC Exam".
+    Take your time with this exam, and remember it is open book. If you do not pass, you will need to wait 3 days to retake so take your time.
+    <em><strong>Returning controllers</strong> who have earned at least an S1 rating previously that are marked as required to take
+        the VATUSA Basic Exam should submit a ticket to VATUSA HQ via the Ticketing system to request a waiver.</em></p>
 
-<p>Until you have completed the Basic ATC Exam, you have been assigned to the Academy facility.  After completion of the
-examination, you will be permitted to select your initial facility.  Please choose carefully, as transfers out of the facility
-will need to be in accordance with division transfer policy.  This policy can be found at www.vatusa.net, Division Info,
-    Policies and then reading "Transferring and Visiting Controllers Policy".</p>
+<p>Until you have completed the Basic ATC Exam, you have been assigned to the Academy facility. After completion of the
+    examination, you will be permitted to select your initial facility. Please choose carefully, as transfers out of the
+    facility will need to be in accordance with division transfer policy. This policy can be found at www.vatusa.net, Division
+    Info, Policies and then reading "Transferring and Visiting Controllers Policy".</p>
 
-<p>If you have any questions, your current point of contact until you transfer to your first facility is VATUSA3.  They
+<p>Finally, questions about training should be done through the VATUSA ticketing system, not e-mail.</p>
+
+<p>If you have any questions, your current point of contact until you transfer to your first facility is VATUSA3. They
     can be reached at vatusa3@vatusa.net.</p>
 
 <p>Good luck!</p>
