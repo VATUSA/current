1b9884acc824225ca283a10a6371b4eb5c19fcbb
Blake Nahin <vatusa6@vatusa.net>
2020-03-14 04:57:26 -0500

    Change CERTSync to idstatus

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,7 +20,8 @@ class Kernel extends ConsoleKernel {
         'App\Console\Commands\UpdateVATSIM',
         'App\Console\Commands\RoleSync',
         'App\Console\Commands\TransferEmails',
-        'App\Console\Commands\ExpireNotices'
+        'App\Console\Commands\ExpireNotices',
+        'App\Console\Commands\CERTCorrect'
     ];
 
     /**
