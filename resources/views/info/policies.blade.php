1238da22b4da5b0ffcd66d87226bd46f6dee7c8f
Blake Nahin <vatusa6@vatusa.net>
2020-03-21 09:21:53 -0700

    Bump 3120.311

--- resources/views/info/policies.blade.php
+++ resources/views/info/policies.blade.php
@@ -55,7 +55,7 @@
                     </tr>
                     <tr>
                         <td>3120.311 &mdash; Procedures for Selection, Submission, and Appointment of Instructors</td>
-                        <td>08/20/2019</td>
+                        <td>03/20/2020</td>
                         @if(\Auth::check())
                             <td><a href="/docs/3120.311.pdf" target="_blank">view</a></td>
                         @else
