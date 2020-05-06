18bfaebbd89cc565bd286a570b226d63524be975
Blake Nahin <vatusa12@vatusa.net>
2020-05-06 03:17:51 -0700

    Load Edit TR

--- public/js/training.js
+++ public/js/training.js
@@ -65,7 +65,7 @@ $(function () {
             method: 'DELETE'
           }).success(() => {
             swal('Success!', 'The training record has been deleted.', 'success')
-            btn.parents('tr').remove()
+            tr.remove()
           })
             .error(err => swal('Error!', 'The training record could not be deleted.' + JSON.stringify(err), 'error'))
         } else {
@@ -74,7 +74,7 @@ $(function () {
       })
 
   })
-  $('.view-record').click(function () {
+  $('.view-tr').click(function () {
     let btn = $(this),
         id  = btn.data('id')
 
@@ -87,18 +87,114 @@ $(function () {
       btn.html('<span class=\'glyphicon glyphicon-eye-open\'></span>').attr('disabled', false)
       console.log(result)
 
+      $('#view-training-record .tr-modal-delete').attr('data-id', id)
+      $('#tr-view-edit').attr('data-id', id)
       $('.training-position').html(result.position)
       $('.training-student').html(result.student.fname + ' ' + result.student.lname)
-      $('#training-artcc').html(result.facility.name);
-      scoreStr = "";
-      for(let i = 1; i <= 5; i++) {
-        scoreStr += "<span class='glyphicon glyphicon-star";
-        scoreStr += i > result.score ? "-empty" : ""
-        scoreStr += "'></span> &nbsp;"
+      $('#training-artcc').html(result.facility.name)
+      scoreStr = ''
+      for (let i = 1; i <= 5; i++) {
+        scoreStr += '<span class=\'glyphicon glyphicon-star'
+        scoreStr += i > result.score ? '-empty' : ''
+        scoreStr += '\'></span> &nbsp;'
       }
       $('#training-score').html(scoreStr)
       $('#training-datetime').html(moment(result.session_date).format('dddd, MMMM Do YYYY, hh:mm'))
-      //Duration
+      let duration    = moment.duration(result.duration),
+          durHours    = duration.hours(),
+          durMinutes  = duration.minutes(),
+          durationStr = ''
+      if (durHours > 0) durationStr += durHours + ' hour' + ((durHours > 1 ? 's' : '')) + ' '
+      if (durMinutes > 0) durationStr += durMinutes + ' minute' + ((durMinutes > 1 ? 's' : ''))
+      $('#training-duration').html(durationStr)
+      $('#training-movements').html(!isNaN(parseInt(result.num_movements)) ? result.movements : '<em>Not Available</em>')
+      let location = ''
+      switch (result.location) {
+        case 0:
+          location = 'Classroom'
+          break
+        case 1:
+          location = 'Live'
+          break
+        case 2:
+          location = 'Sweatbox'
+          break
+        default:
+          location = '<em>Not Available</em>'
+          break
+      }
+      $('#training-location').html(location)
+      $('#training-instructor').html(result.instructor.fname + ' ' + result.instructor.lname)
+      $('#training-notes').html(result.notes)
+
+      $('#view-training-record').modal('show')
+    })
+      .fail((xhr, status, error) => {
+        btn.html('<span class=\'glyphicon glyphicon-eye-open\'></span>').attr('disabled', false)
+        alert('Unable to get training record. ' + error)
+      })
+  })
+  $('.edit-tr').click(function () {
+    let btn = $(this),
+        id  = btn.data('id')
+
+    btn.html('<i class=\'fas fa-spinner fa-spin\'></i>').attr('disabled', true)
+
+    $.ajax({
+      url   : $.apiUrl() + '/v2/training/record/' + id,
+      method: 'GET',
+    }).done(result => {
+      btn.html('<span class=\'glyphicon glyphicon-eye-open\'></span>').attr('disabled', false)
+      console.log(result)
+
+      $('#edit-training-record .tr-modal-delete, #e-training-submit').attr('data-id', id)
+      $('input.e-training-position').val(result.position)
+      $('span.e-training-position').html(result.position)
+      $('#e-training-student').html(result.student.fname + ' ' + result.student.lname)
+      $('#e-training-artcc').html(result.facility.name)
+      $('#e-training-score').val(result.score)
+      $('#e-training-datetime').val(moment(result.session_date).format('MM/DD/YYYY HH:mm'))
+      $('#e-training-duration-hrs').val(moment.duration(result.duration).hours())
+      $('#e-training-duration-mins').val(moment.duration(result.duration).minutes())
+      $('#e-training-movements').val(result.num_movements)
+      $('#e-training-location').val(result.location)
+      $('#e-training-instructor').html(result.instructor.fname + ' ' + result.instructor.lname)
+     // $('#e-training-notes').html(result.notes)
+
+      $('#e-training-datetime').datetimepicker({
+        timepicker: true,
+        format    : 'm/d/Y H:i',
+        mask      : true,
+        maxDate   : '+1970/01/01',
+        step      : 15
+      })
+
+      tinymce.init({
+        selector                     : '#e-training-notes',
+        plugins                      : 'preview paste importcss searchreplace autolink autosave save visualblocks visualchars fullscreen image link media template table charmap hr nonbreaking toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap emoticons',
+        imagetools_cors_hosts        : ['picsum.photos'],
+        menubar                      : 'file edit view insert format tools table help',
+        toolbar                      : 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | charmap emoticons | fullscreen preview save | image media link',
+        toolbar_sticky               : true,
+        autosave_ask_before_unload   : true,
+        autosave_interval            : '30s',
+        autosave_prefix              : '{path}{query}-{id}-',
+        autosave_restore_when_empty  : false,
+        autosave_retention           : '2m',
+        image_advtab                 : true,
+        importcss_append             : true,
+        height                       : 600,
+        image_caption                : true,
+        noneditable_noneditable_class: 'mceNonEditable',
+        toolbar_mode                 : 'sliding',
+        contextmenu                  : 'link image imagetools table',
+        setup : editor => {
+          editor.on('init', e => {
+            editor.setContent(result.notes);
+          })
+        }
+      })
+      $('#edit-training-record').modal('show')
 
     })
       .fail((xhr, status, error) => {
@@ -106,6 +202,52 @@ $(function () {
         alert('Unable to get training record. ' + error)
       })
 
-    $('#view-training-record').modal('show')
   })
+  $('#edit-training-record').on('hidden.bs.modal', function () {
+    tinymce.get('e-training-notes').remove()
+  })
+
+  $('.tr-modal-delete').click(function () {
+    let btn = $(this),
+        id  = btn.data('id'),
+        tr  = $('.delete-tr[data-id=' + id + ']').parents('tr')
+    let alertText = document.createElement('table')
+    alertText.className = 'table table-border'
+    alertText.innerHTML = $('.training-records-list').first().children('thead').ignore('.alert-ignore').html()
+      + '<tbody><tr>' + tr.ignore('.alert-ignore').html() + '</tr></tbody>'
+
+    swal({
+      title     : 'Deleting Training Record',
+      content   : alertText,
+      text      : 'Are you sure you want to delete the training record?',
+      icon      : 'warning',
+      buttons   : {
+        cancel : {
+          text   : 'No, cancel',
+          visible: true,
+        },
+        confirm: {
+          text      : 'Yes, delete',
+          closeModal: false
+        }
+      },
+      dangerMode: true,
+    })
+      .then(willDelete => {
+        if (willDelete) {
+          $.ajax({
+            url   : $.apiUrl() + '/v2/training/record/' + id,
+            method: 'DELETE'
+          }).success(() => {
+            swal('Success!', 'The training record has been deleted.', 'success')
+            tr.remove()
+            btn.parents('.modal').modal('hide')
+          })
+            .error(err => swal('Error!', 'The training record could not be deleted.' + JSON.stringify(err), 'error'))
+        } else {
+          return false
+        }
+      })
+  })
+
 })
