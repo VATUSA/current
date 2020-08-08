bfbe1293820a6b97a16373fce939a6247b62727d
Blake Nahin <vatusa12@vatusa.net>
2020-08-08 16:28:29 -0500

    Fix typo

--- app/User.php
+++ app/User.php
@@ -287,9 +287,9 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 
         // S1-C1 within 90 check
         $promotion = Promotions::where('cid', $this->cid)->where([
-             'to',         '<=', Helper::ratingIntFromShort("C1"),
-             'to',         '>', 'from'
-             'created_at', '>=', \DB::raw("DATE(NOW() - INTERVAL 90 DAY)")
+             ['to',         '<=', Helper::ratingIntFromShort("C1")],
+             ['to',         '>', 'from'],
+             ['[created_at', '>=', \DB::raw("DATE(NOW() - INTERVAL 90 DAY)")]
              ])->first();
         if ($promotion == null) {
             $checks['promo'] = 1;
