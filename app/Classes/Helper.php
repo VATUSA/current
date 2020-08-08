e9d5e8d78ff9d75b338cd653e567099dbbd51f22
Blake Nahin <bnahin@live.com>
2020-08-08 04:37:55 -0500

    OTS Attempt and Recommending Parties

--- app/Classes/Helper.php
+++ app/Classes/Helper.php
@@ -87,4 +87,41 @@ class Helper
 
         return ($facility != null ? $facility->name : 'Unknown');
     }
+
+    public static function numToOrdinalWord($num)
+    {
+        $first_word = array(
+            'Zeroeth',
+            'First',
+            'Second',
+            'Third',
+            'Fouth',
+            'Fifth',
+            'Sixth',
+            'Seventh',
+            'Eighth',
+            'Ninth',
+            'Tenth',
+            'Elevents',
+            'Twelfth',
+            'Thirteenth',
+            'Fourteenth',
+            'Fifteenth',
+            'Sixteenth',
+            'Seventeenth',
+            'Eighteenth',
+            'Nineteenth',
+            'Twentieth'
+        );
+        $second_word = array('', '', 'Twenty', 'Thirty', 'Forty', 'Fifty');
+
+        if ($num <= 20) {
+            return $first_word[$num];
+        }
+
+        $first_num = substr($num, -1, 1);
+        $second_num = substr($num, -2, 1);
+
+        return $string = str_replace('y-eth', 'ieth', $second_word[$second_num] . '-' . $first_word[$first_num]);
+    }
 }
