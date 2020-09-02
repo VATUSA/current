e812b7a517f8f55d970083df3440a60ebe600c4f
Blake Nahin <bnahin@live.com>
2020-09-02 01:54:43 -0500

    Logout non-testers

--- app/Http/Middleware/AuthLastActivity.php
+++ app/Http/Middleware/AuthLastActivity.php
@@ -1,26 +1,35 @@
 <?php namespace App\Http\Middleware;
 
+use App\Classes\RoleHelper;
+use Auth;
 use Closure;
 use Carbon\Carbon;
 
-class AuthLastActivity {
+class AuthLastActivity
+{
 
-	/**
-	 * Handle an incoming request.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Closure  $next
-	 * @return mixed
-	 */
-	public function handle($request, Closure $next)
-	{
-	    if (\Auth::check()) {
-	        $user = \Auth::user();
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Auth::check()) {
+            $user = Auth::user();
             $user->lastactivity = Carbon::now();
             $user->save();
+
+            if (app()->environment("livedev") && !RoleHelper::isVATUSAStaff($user->cid) && !in_array($user->cid,
+                    explode(',', env("LIVEDEV_CIDS", "")))) {
+                return redirect()->to('/logout');
+            }
         }
 
-		return $next($request);
-	}
+        return $next($request);
+    }
 
 }
