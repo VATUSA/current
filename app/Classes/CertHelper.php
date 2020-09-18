735af5facfbddff47e59e1b8a50472d3ed9e4d29
Blake Nahin <vatusa6@vatusa.net>
2020-09-18 12:09:21 -0500

    Prevent persistence of CERT

--- app/Classes/CertHelper.php
+++ app/Classes/CertHelper.php
@@ -1,8 +1,10 @@
 <?php namespace App\Classes;
 
+use App\Helpers\RatingHelper;
 use App\User;
 
-class CertHelper {
+class CertHelper
+{
     private static $baseUrl = "http://cert.vatsim.net/vatsimnet/admin/";
     private static $authId;
     private static $authPassword;
@@ -11,11 +13,14 @@ class CertHelper {
     // Returns array of lines for processing
     public static function downloadDivision()
     {
-        if (env('CERT_ID') == 0) return;
+        if (env('CERT_ID') == 0) {
+            return;
+        }
         $url = static::buildUrl("divdb.php");
         $result = file_get_contents($url);
         $lines = array();
         $lines = explode("\n", $result);
+
         return $lines;
     }
 
@@ -24,13 +29,34 @@ class CertHelper {
     // newRating can be integer or short.
     public static function changeRating($cid, $newRating, $addToDatabase = false)
     {
-        if (env('CERT_ID') == 0) return 1;
+        if (env('APP_ENV', 'dev') != "prod") {
+            if ($addToDatabase) {
+                $user = User::find($cid);
+                $user->rating = $newRating;
+                $user->save();
 
-        if (!is_numeric($newRating)) { $newRating = Helper::ratingIntFromShort($newRating); }
+                $action = new \App\Actions();
+                $action->to = $cid;
+                $action->log = "Rating set to " . \App\Classes\Helper::ratingShortFromInt($newRating) . " by " . \Auth::user()->fullname() . " (" . \Auth::user()->cid . ")";
+                $action->save();
+            }
+
+            # Don't do anything
+            return 1;
+        }
+
+        if (env('CERT_ID') == 0) {
+            return 1;
+        }
+
+        if (!is_numeric($newRating)) {
+            $newRating = Helper::ratingIntFromShort($newRating);
+        }
         $url = static::buildUrl("ratch.php", array('id' => $cid, 'rat' => $newRating));
         $result = file_get_contents($url);
-        if ($result == "ERR:NoExist" || $result == "ERR:NotAllowed")
+        if ($result == "ERR:NoExist" || $result == "ERR:NotAllowed") {
             return 0;
+        }
 
         if ($addToDatabase) {
             $user = User::find($cid);
@@ -49,12 +75,15 @@ class CertHelper {
     private static function buildUrl($url, $args = array())
     {
         $queryString = "";
-        foreach ($args as $key => $value)
-        {
-            if ($queryString != "") $queryString .= "&";
+        foreach ($args as $key => $value) {
+            if ($queryString != "") {
+                $queryString .= "&";
+            }
             $queryString .= "$key=$value";
         }
-        if ($queryString) $queryString .= "&";
+        if ($queryString) {
+            $queryString .= "&";
+        }
         $queryString .= "authid=" . env('CERT_ID') . "&authpassword=" . env('CERT_PASSWORD');
 
         return static::$baseUrl . $url . "?" . $queryString;
