faa1f54867855d2f12ecaee17f1da340498ea47b
Blake Nahin <vatusa12@vatusa.net>
2020-10-09 16:41:03 -0500

    CSS versions

--- resources/views/mgt/controller/training/otsEval.blade.php
+++ resources/views/mgt/controller/training/otsEval.blade.php
@@ -1,7 +1,7 @@
 @push('styles')
     <link rel="stylesheet" type="text/css"
           href="https://cdn.datatables.net/v/bs/jszip-2.5.0/dt-1.10.20/b-1.6.1/b-colvis-1.6.1/b-flash-1.6.1/b-html5-1.6.1/fh-3.1.6/kt-2.5.1/r-2.2.3/rg-1.1.1/sc-2.0.1/sp-1.0.1/sl-1.3.1/datatables.min.css"/>
-    <link rel="stylesheet" type="text/css" href="{{ secure_asset('css/otsEval.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('css/otsEval-v2.3.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ secure_asset("datetimepicker/datetimepicker.css") }}">
 @endpush
 
