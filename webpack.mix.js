9299cd45b419786201027f0b50ad2c46f738890c
Blake Nahin <vatusa12@vatusa.net>
2020-10-11 12:57:40 -0500

    Promotion page button and width

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,10 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/email.js', 'public/js')
   .js('resources/assets/js/privacy.js', 'public/js')
-  .version();
+  //.js('resources/assets/js/vatusa.js', 'public/js')
+  //.babel('resources/assets/js/training.js', 'public/js/training.js')
+  .sass('resources/assets/sass/vatusa.scss', 'public/css')
+
+if (mix.inProduction()) {
+  mix.version();
+}
