c7b907172ed5bd58958955cf5a83ce51a755f96e
Michael Romashov <mromashov@icloud.com>
2020-10-20 18:15:43 -0400

    Added search autocomplete to member management.

--- resources/views/mgt/controller/index.blade.php
+++ resources/views/mgt/controller/index.blade.php
@@ -139,7 +139,7 @@
                 <h3 class="panel-title">
                     <div class="row">
                         <div class="col-md-8" style="font-size: 16pt;">{{$user->fname}} {{$user->lname}} - {{$user->cid}}</div>
-                        <form class="form-inline">
+                        <form class="form-inline" id="controllerForm">
                             <div class="col-md-4 text-right form-group">
                                 <input type="text" id="cidsearch" class="form-control" placeholder="CID">
                                 <button type="button" class="btn btn-primary" id="cidsearchbtn"><i
@@ -623,7 +623,7 @@
             </div>
         </div>
     </div>
-
+    <script src="/js/jquery.autocomplete.js"></script>
     <script>
       function viewXfer (id) {
         $.get("{{secure_url('mgt/ajax/transfer/reason')}}", {id: id}, function (data) {
@@ -672,6 +672,31 @@
           $(this).find('i').addClass('glyphicon-chevon-down').addClass('glyphicon-chevron-up')
         }
       })
+
+      $('#cidsearch').devbridgeAutocomplete({
+          lookup: [],
+          onSelect: (suggestion) => {
+              $('#cidsearch').val(suggestion.data);
+              $('#controllerForm').submit();
+          }
+      });
+      var prevVal = '';
+      $('#cidsearch').on('change keydown keyup paste', function() {
+          let newVal = $(this).val();
+          if (newVal.length === 4 && newVal !== prevVal) {
+              let url = '/v2/user/' + (isNaN(newVal) ? 'filterlname/' : 'filtercid/');
+              prevVal = newVal;
+              $.get($.apiUrl() + url + newVal)
+              .success((data) => {
+                  $('#cidsearch').devbridgeAutocomplete().setOptions({
+                      lookup: $.map(data, (item) => {
+                          return { value: item.fname + ' ' + item.lname + ' (' + item.cid + ')', data: item.cid };
+                      })
+                  });
+                  $('#cidsearch').focus();
+              });
+          }
+      });
     </script>
 
 
