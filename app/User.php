9be3cdcc7ae4fa83a5909e6327ce82dbbfdd403a
Michael Romashov <mromashov@icloud.com>
2020-11-27 12:14:23 -0500

    Fixed transfer eligibility check for I1+

--- app/User.php
+++ app/User.php
@@ -303,9 +303,8 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
         // S1-C1 within 90 check
         $promotion = Promotions::where('cid', $this->cid)->where([
             ['to', '<=', Helper::ratingIntFromShort("C1")],
-            ['to', '>', 'from'],
             ['created_at', '>=', \DB::raw("DATE(NOW() - INTERVAL 90 DAY)")]
-        ])->first();
+        ])->whereRaw('promotions.to > promotions.from')->first();
         if ($promotion == null) {
             $checks['promo'] = 1;
         } else {
