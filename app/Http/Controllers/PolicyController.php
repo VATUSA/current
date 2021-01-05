87565f5b8c5ce7d491084fc592e3de09e035d1f4
Blake Nahin <bnahin@live.com>
2021-01-05 02:41:40 -0800

    Begin Policy Uploads, create TrainingController

---
+++ app/Http/Controllers/PolicyController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Policy;
+use Illuminate\Http\Request;
+
+class PolicyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Foundation\Application|\Illuminate\Http\Response|\Illuminate\View\View
+     */
+    public function index()
+    {
+        return view('info.policies');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Policy  $policy
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Policy $policy)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Policy  $policy
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Policy $policy)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Policy  $policy
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Policy $policy)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Policy  $policy
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Policy $policy)
+    {
+        //
+    }
+}
