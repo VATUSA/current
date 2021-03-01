cf5e2d58e09c30429adb6f4546fee90abbef9103
Blake Nahin <vatusa12@vatusa.net>
2021-03-01 01:06:44 -0600

    Remove APIv1

--- config/app.php
+++ config/app.php
@@ -153,7 +153,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        \SocialiteProviders\Manager\ServiceProvider::class,
+        //\SocialiteProviders\Manager\ServiceProvider::class,
 
     ],
 
