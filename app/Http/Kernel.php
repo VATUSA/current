7bbf787b54430834484bbf2a2222bd9134f668a2
Daniel Hawton <daniel@hawton.org>
2021-06-06 20:17:53 -0500

    Upgrade to Laravel 5.7

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -16,7 +16,6 @@ class Kernel extends HttpKernel
         'Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse',
         'Illuminate\Session\Middleware\StartSession',
         'Illuminate\View\Middleware\ShareErrorsFromSession',
-        \Barryvdh\Cors\HandleCors::class,
     ];
 
     /**
