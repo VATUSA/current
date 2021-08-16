3c24488ecb4bafd656d5bec06eff6c51b9d6254e
Blake Nahin <bnahin@live.com>
2021-08-16 05:23:35 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-cat /run/secrets/key > /www/.env
-cat /run/secrets/*.env >> /www/.env
+# cat /run/secrets/key > /www/.env
+# cat /run/secrets/*.env >> /www/.env
 
 chown application:application /www/.env
 
@@ -11,4 +11,8 @@ mkdir /www/storage/framework/views
 mkdir /www/storage/framework/session
 chown -R application:application /www/storage
 
-cd /www && php artisan migrate
+if [[ "$ENV" == "prod" ]] || [[ "$ENV" == "livedev" ]] || [[ "$ENV" == "staging" ]]; then
+  # echo "*    *    *     *     *    cd /www && php artisan schedule:run" >> /etc/crontabs/application
+  cd /www && php artisan migrate
+  chmod -R 775 /www/storage/logs
+fi
