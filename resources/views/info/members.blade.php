60fac6af7035ae2a9a2570636d786ff1addc2a71
Jon <jonhasacat8@gmail.com>
2022-11-18 23:31:49 -0800

    Remove USA10 position

--- resources/views/info/members.blade.php
+++ resources/views/info/members.blade.php
@@ -91,13 +91,13 @@
                                     <td>{{\App\Classes\RoleHelper::roleTitle("US9")}}</td>
                                     <td><a href="mailto:vatusa9@vatusa.net"><i class="fa fa-envelope"></i></a></td>
                                 </tr>
+                                <!--
                                 <tr>
                                     <td>VATUSA10</td>
                                     <td>{{\App\Classes\RoleHelper::getNameFromRole("US10")}}</td>
                                     <td>{{\App\Classes\RoleHelper::roleTitle("US10")}}</td>
                                     <td><a href="mailto:vatusa10@vatusa.net"><i class="fa fa-envelope"></i></a></td>
                                 </tr>
-                                <!--
                                 <tr>
                                     <td>VATUSA11</td>
                                     <td>{{\App\Classes\RoleHelper::getNameFromRole("US11")}}</td>
