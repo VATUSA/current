9b963e880ddbe3af93e609ca134d03441442a402
Jon <jonhasacat8@gmail.com>
2023-03-11 12:22:02 -0800

    Discord bot nudge has to be a POST

--- app/Classes/DiscordHelper.php
+++ app/Classes/DiscordHelper.php
@@ -3,12 +3,14 @@
 namespace App\Classes;
 
 use App\Models\User;
+use GuzzleHttp\Client as Guzzle;
 
 class DiscordHelper
 {
     public static function assignRoles($cid) {
+        $guzzle = new Guzzle();
         $user = User::where('cid', $cid)->first();
         if ($user->discord_id != null)
-            file_get_contents("https://bot.vatusa.net/assignRoles/" . $user->discord_id);
+            $guzzle->post("https://bot.vatusa.net/assignRoles/" . $user->discord_id);
     }
 }
