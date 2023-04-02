ab6513f7bd81dc63e125a0e1e184da8c660478f7
Jon <jonhasacat8@gmail.com>
2023-04-01 19:39:20 -0700

    Add "Don't Promote" button so people aren't tempted to hit Promote.

--- resources/views/mgt/controller/promotion.blade.php
+++ resources/views/mgt/controller/promotion.blade.php
@@ -29,7 +29,7 @@
                                 <label class="col-sm-3 control-label">Rating</label>
                                 <p class="col-sm-9 form-control-static">
                                     {{ \App\Classes\Helper::ratingShortFromInt($user->rating) }} <i
-                                        class="fa fa-arrow-right text-success" style="padding:0 10px;"></i>
+                                            class="fa fa-arrow-right text-success" style="padding:0 10px;"></i>
                                     <strong>{{ \App\Classes\Helper::ratingShortFromInt($user->rating + 1) }}</strong>
                                 </p>
                             </div>
@@ -65,19 +65,20 @@
                                 <p class="col-sm-9 form-control-static">
                                     @switch($trainingRecordStatus)
                                         @case(1)
-                                        <span class="label label-success" style="font-size:90%;"><span
-                                                class="glyphicon glyphicon-ok"></span> Exists</span>
-                                        @break
+                                            <span class="label label-success" style="font-size:90%;"><span
+                                                        class="glyphicon glyphicon-ok"></span> Exists</span>
+                                            @break
                                         @case(0)
-                                        <span class="label label-danger" style="font-size:90%;" rel="tooltip"
-                                              title="There must be a training record present in the CTRS that is marked OTS Pass under the specified position." onclick="window.open('/mgt/controller/{{ $user->cid }}#training', 'blank')"><span
-                                                class="glyphicon glyphicon-remove"></span> Does Not Exist</span>
-                                        @break
+                                            <span class="label label-danger" style="font-size:90%;" rel="tooltip"
+                                                  title="There must be a training record present in the CTRS that is marked OTS Pass under the specified position."
+                                                  onclick="window.open('/mgt/controller/{{ $user->cid }}#training', 'blank')"><span
+                                                        class="glyphicon glyphicon-remove"></span> Does Not Exist</span>
+                                            @break
                                         @default
-                                        <span class="label label-default" style="font-size:90%;"><i
-                                                class="fas fa-times-circle"></i> Not Applicable</span>
-                                        <br>
-                                        @break
+                                            <span class="label label-default" style="font-size:90%;"><i
+                                                        class="fas fa-times-circle"></i> Not Applicable</span>
+                                            <br>
+                                            @break
                                     @endswitch
                                 </p>
                             </div>
@@ -88,23 +89,23 @@
                                 <p class="col-sm-9 form-control-static">
                                     @switch($otsEvalStatus)
                                         @case(-1)
-                                        <span class="label label-default" style="font-size:90%;"><i
-                                                class="fas fa-times-circle"></i> Not Applicable</span>
-                                        <br>
-                                        @break
+                                            <span class="label label-default" style="font-size:90%;"><i
+                                                        class="fas fa-times-circle"></i> Not Applicable</span>
+                                            <br>
+                                            @break
                                         @case(0)
-                                        <span class="label label-danger" style="font-size:90%;" rel="tooltip"
-                                              title="The OTS evalulation form must be completely and correctly filled out on the right."><span
-                                                class="glyphicon glyphicon-remove"></span> Not Complete</span>
-                                        @break
+                                            <span class="label label-danger" style="font-size:90%;" rel="tooltip"
+                                                  title="The OTS evalulation form must be completely and correctly filled out on the right."><span
+                                                        class="glyphicon glyphicon-remove"></span> Not Complete</span>
+                                            @break
                                         @case(1)
-                                        <span class="label label-success" style="font-size:90%;"><span
-                                                class="glyphicon glyphicon-ok"></span> Complete</span>
-                                        @break
+                                            <span class="label label-success" style="font-size:90%;"><span
+                                                        class="glyphicon glyphicon-ok"></span> Complete</span>
+                                            @break
                                         @case(2)
-                                        <span class="label label-warning" style="font-size:90%;"><span
-                                                class="glyphicon glyphicon-remove"></span> Not Passed</span>
-                                        @break
+                                            <span class="label label-warning" style="font-size:90%;"><span
+                                                        class="glyphicon glyphicon-remove"></span> Not Passed</span>
+                                            @break
                                     @endswitch
                                 </p>
                             </div>
@@ -112,8 +113,12 @@
                                 <div class="col-sm-3 col-sm-offset-3">
                                     <button type="button" id="submit-promotion" class="btn btn-success btn-block"
                                             style="width:150px;"
-                                             @if(!($otsEvalStatus == 1 /* && $trainingRecordStatus == 1*/)) disabled @endif>
+                                            @if(!($otsEvalStatus == 1 /* && $trainingRecordStatus == 1*/)) disabled @endif>
                                         <i class="fas fa-check"></i> Promote
+                                    </button>
+                                    <button type="button" id="cancel-promotion" class="btn btn-danger btn-block"
+                                            style="width:150px;">
+                                        <i class="fas fa-check"></i> Don't Promote
                                     </button>
                                 </div>
                             </div>
@@ -143,12 +148,13 @@
                                     <h4 class="list-group-item-heading">{{ $form->name }}
                                         @if($dateOfExam && $form->rating_id == $user->rating + 1)
                                             <span
-                                                class="glyphicon glyphicon-ok pull-right"></span>
-                                        @elseif($dateOfExam) <span
-                                            class="glyphicon glyphicon-remove pull-right"></span>
+                                                    class="glyphicon glyphicon-ok pull-right"></span>
+                                        @elseif($dateOfExam)
+                                            <span
+                                                    class="glyphicon glyphicon-remove pull-right"></span>
                                         @else
                                             <span
-                                                class="glyphicon glyphicon-{{$form->is_statement ? 'share' : 'arrow-right'}} pull-right"></span>
+                                                    class="glyphicon glyphicon-{{$form->is_statement ? 'share' : 'arrow-right'}} pull-right"></span>
                                         @endif
                                     </h4>
                                     <p class="list-group-item-text">{!! $form->description !!}</p>
@@ -165,96 +171,99 @@
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script type="text/javascript" src="{{ secure_asset("datetimepicker/datetimepicker.js") }}"></script>
     <script type="text/javascript">
-      $(document).ready(function () {
-        /*$('[name=examiner]').autocomplete({
-          source   : '/ajax/cid',
-          minLength: 2,
-          select   : function (event, ui) {
-            $('[name=examiner]').val(ui.item.value)
+        $(document).ready(function () {
+            /*$('[name=examiner]').autocomplete({
+              source   : '/ajax/cid',
+              minLength: 2,
+              select   : function (event, ui) {
+                $('[name=examiner]').val(ui.item.value)
 
-            return false
-          }
-        })*/
-
-        const jsDate = new Date(),
-              offset = jsDate.getUTCFullYear() - jsDate.getFullYear() + 2
-
-        $('#exam-date').datetimepicker({
-          timepicker: false,
-          format    : 'Y-m-d',
-          mask      : true,
-          maxDate   : '+1970/01/0' + offset,
-          step      : 15
-        })
-
-        $('#position').keyup(function () {
-          $(this).val($(this).val().toUpperCase())
-          $('.list-group-item:not(.disabled)').addClass('disabled')
-          let pos = $(this).val().split('_')
-          if ((pos.length === 2 || pos.length === 3) && (pos.length >= 2 && pos[pos.length - 1].length === 3)) {
-            pos = pos.pop()
-            if (pos !== '') {
-              $('#submit-promotion').attr('disabled', false)
-              $('#' + pos.toLowerCase() + '.temp-disabled').removeClass('disabled')
-            } else $('#submit-promotion').attr('disabled', true)
-          } else
-            $('#submit-promotion').attr('disabled', true)
-        })
-
-        $('.eval-link').click(function (e) {
-          e.preventDefault()
-          if ($(this).hasClass('disabled')) return
-          const link        = $(this),
-                id          = link.data('id'),
-                statement   = link.data('statement'),
-                description = link.find('p').html()
-          if (statement) {
-            return swal({
-              title  : 'Delivery and Ground Certification Statement',
-              text   : description,
-              icon   : 'info',
-              buttons: {
-                confirm: {
-                  text     : 'eSign and Certify',
-                  className: 'btn-success'
-                }
+                return false
               }
-            }).then(r => {
-              if (r) {
-                swal('DEL/GND Certification Complete', 'You may now proceed with the promotion.', 'success')
-                $('#submit-promotion').attr('disabled', false)
-                $('a.list-group-item[data-id="' + id + '"]').removeClass('list-group-item-info disabled').addClass('list-group-item-success')
-                  .find('span.glyphicon').removeClass('glyphicon-share').addClass('glyphicon-ok')
-              }
+            })*/
+
+            const jsDate = new Date(),
+                offset = jsDate.getUTCFullYear() - jsDate.getFullYear() + 2
+
+            $('#exam-date').datetimepicker({
+                timepicker: false,
+                format: 'Y-m-d',
+                mask: true,
+                maxDate: '+1970/01/0' + offset,
+                step: 15
             })
-          }
-          Cookies.set('eval-pos', $('#position').val())
-          Cookies.set('eval-date', $('#exam-date').val())
-          window.location = '{{ $dateOfExam ? "/mgt/facility/training/eval/$evalId/view" : 'eval'}}'
-        })
 
-        $('#submit-promotion').click(function (e) {
-          let btn = $(this)
-          btn.attr('disabled', true).html('<i class="fas fa-spin fa-spinner"></i> Promoting...')
-          e.preventDefault()
-          $.post($.apiUrl() + "/v2/user/{{ $user->cid }}/rating", {
-            cid     : {{ $user->cid }},
-            rating  : {{ $user->rating + 1 }},
-            examDate: $('#exam-date').val(),
-            position: $('#position').val()
-          }, result => {
-            btn.attr('disabled', false).html('<i class="fas fa-check"></i> Promote')
-            if (result.data.hasOwnProperty('status') && result.data.status === 'OK') {
-              return swal('Success!', 'The controller has been promoted.', 'success').then(() => {
-                return window.location = '{{ secure_url('/mgt/facility') }}'
-              })
-            } else
-              return swal('Error!', 'The controller was not promoted. ' + result.data.msg, 'error')
-          }).fail(_ => {
-            btn.attr('disabled', false).html('<i class="fas fa-check"></i> Promote')
-            return swal('Error!', 'The controller was not promoted. Please ensure all fields and requirements are completed or try again later.', 'error')
-          })
+            $('#position').keyup(function () {
+                $(this).val($(this).val().toUpperCase())
+                $('.list-group-item:not(.disabled)').addClass('disabled')
+                let pos = $(this).val().split('_')
+                if ((pos.length === 2 || pos.length === 3) && (pos.length >= 2 && pos[pos.length - 1].length === 3)) {
+                    pos = pos.pop()
+                    if (pos !== '') {
+                        $('#submit-promotion').attr('disabled', false)
+                        $('#' + pos.toLowerCase() + '.temp-disabled').removeClass('disabled')
+                    } else $('#submit-promotion').attr('disabled', true)
+                } else
+                    $('#submit-promotion').attr('disabled', true)
+            })
+
+            $('.eval-link').click(function (e) {
+                e.preventDefault()
+                if ($(this).hasClass('disabled')) return
+                const link = $(this),
+                    id = link.data('id'),
+                    statement = link.data('statement'),
+                    description = link.find('p').html()
+                if (statement) {
+                    return swal({
+                        title: 'Delivery and Ground Certification Statement',
+                        text: description,
+                        icon: 'info',
+                        buttons: {
+                            confirm: {
+                                text: 'eSign and Certify',
+                                className: 'btn-success'
+                            }
+                        }
+                    }).then(r => {
+                        if (r) {
+                            swal('DEL/GND Certification Complete', 'You may now proceed with the promotion.', 'success')
+                            $('#submit-promotion').attr('disabled', false)
+                            $('a.list-group-item[data-id="' + id + '"]').removeClass('list-group-item-info disabled').addClass('list-group-item-success')
+                                .find('span.glyphicon').removeClass('glyphicon-share').addClass('glyphicon-ok')
+                        }
+                    })
+                }
+                Cookies.set('eval-pos', $('#position').val())
+                Cookies.set('eval-date', $('#exam-date').val())
+                window.location = '{{ $dateOfExam ? "/mgt/facility/training/eval/$evalId/view" : 'eval'}}'
+            })
+
+            $('#submit-promotion').click(function (e) {
+                let btn = $(this)
+                btn.attr('disabled', true).html('<i class="fas fa-spin fa-spinner"></i> Promoting...')
+                e.preventDefault()
+                $.post($.apiUrl() + "/v2/user/{{ $user->cid }}/rating", {
+                    cid: {{ $user->cid }},
+                    rating: {{ $user->rating + 1 }},
+                    examDate: $('#exam-date').val(),
+                    position: $('#position').val()
+                }, result => {
+                    btn.attr('disabled', false).html('<i class="fas fa-check"></i> Promote')
+                    if (result.data.hasOwnProperty('status') && result.data.status === 'OK') {
+                        return swal('Success!', 'The controller has been promoted.', 'success').then(() => {
+                            return window.location = '{{ secure_url('/mgt/facility') }}'
+                        })
+                    } else
+                        return swal('Error!', 'The controller was not promoted. ' + result.data.msg, 'error')
+                }).fail(_ => {
+                    btn.attr('disabled', false).html('<i class="fas fa-check"></i> Promote')
+                    return swal('Error!', 'The controller was not promoted. Please ensure all fields and requirements are completed or try again later.', 'error')
+                })
+            });
+            $('#cancel-promotion').click(function (e) {
+                window.location = '{{ secure_url('/mgt/facility') }}'
+            });
         })
-      })
     </script>
 @endsection
