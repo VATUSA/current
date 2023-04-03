93a0ed108147e36dc0e112f0cbca3dac2b5e08a9
Jon <jonhasacat8@gmail.com>
2023-04-03 15:52:35 -0700

    Revert "Disable Fruitcake CORS, update composer packages."
    This reverts commit 6865a19cbebc60c2c6ed92c7ab11222485153585.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -11,7 +11,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-//        \Fruitcake\Cors\HandleCors::class,
+        \Fruitcake\Cors\HandleCors::class,
         'Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode',
 //        'Illuminate\Cookie\Middleware\EncryptCookies',
         'Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse',
