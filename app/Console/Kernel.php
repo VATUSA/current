926153d70e2946c67fdca8cd73e127c8ccc6b888
Jon <jonhasacat8@gmail.com>
2023-04-15 19:44:08 -0700

    Run DiscordSync every hour

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -43,6 +43,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('rolesync')->cron('45 * * * *');
         $schedule->command('ntos:expire')->weekly();
         $schedule->command('promos:cacheeligible')->dailyAt('05:00');
+        $schedule->command('discord:rolesync')->hourly();
     }
 
 }
