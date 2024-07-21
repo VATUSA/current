58604fe8c8d11702ccbc7f3a7b9543486da90fbc
Raaj Patel <41763998+Raajheer1@users.noreply.github.com>
2024-07-20 19:22:45 -0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -48,6 +48,7 @@ return [
         'mysql'  => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
+            'port'      => env('DB_PORT', '3306'),
             //'host'      => [env('DB_HOST2', 'localhost'), env('DB_HOST1','')],
             'database'  => env('DB_DATABASE', 'forge'),
             'username'  => env('DB_USERNAME', 'forge'),
