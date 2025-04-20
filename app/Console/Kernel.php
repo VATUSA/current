598e641171ac2c6e955a204d3d1bedfd9cfb9fb0
Raaj Patel <raaj.patel229@gmail.com>
2025-04-20 11:11:53 -0500

    Add logging

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -34,12 +34,20 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('CERTSync')->twiceDaily(11, 23);
-        $schedule->command('UpdateVATSIM')->everyMinute()->onOneServer()->withoutOverlapping(1);
+        $schedule->command('UpdateVATSIM')->everyMinute()->withoutOverlapping(1);
         $schedule->command('TattlerTransfers')->cron('15 0 * * *')->onOneServer()->withoutOverlapping();
         // $schedule->command("TattlerStaffVisit")->weekly()->sundays()->at("23:00");
         // $schedule->command('rolesync')->cron('45 * * * *');
         $schedule->command('ntos:expire')->weekly()->onOneServer()->withoutOverlapping();
         $schedule->command('promos:cacheeligible')->dailyAt('05:00')->onOneServer()->withoutOverlapping();
         $schedule->command('vatsim:sync')->hourly()->onOneServer()->withoutOverlapping();
+
+        $schedule->before(function ($event) {
+            logger('About to run: ' . $event->command);
+        });
+
+        $schedule->after(function ($event) {
+            logger('Finished running: ' . $event->command);
+        });
     }
 }
