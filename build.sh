9c78bcf47d5d530e3cc679b50dd2aff2f38aefcc
Jon <jonhasacat8@gmail.com>
2026-01-06 17:34:55 -0800

    Fix sh file insanity

--- build.sh
+++ build.sh
@@ -7,11 +7,12 @@ chown application:application /www/.env
 
 # echo "*    *    *     *     *    cd /www && php artisan schedule:run" > /etc/crontabs/application
 
-mkdir /www/storage/framework/views
-mkdir /www/storage/framework/session
+mkdir -p /www/storage/framework/views
+mkdir -p /www/storage/framework/session
 chown -R application:application /www/storage
 
-if [[ "$ENV" == "prod" ]] || [[ "$ENV" == "livedev" ]] || [[ "$ENV" == "staging" ]]; then
+if [[ "$ENV" == "prod" ]] || [[ "$ENV" == "livedev" ]] || [[ "$ENV" == "staging" ]]
+then
   # echo "*    *    *     *     *    cd /www && php artisan schedule:run" >> /etc/crontabs/application
   cd /www && php artisan migrate
   chmod -R 775 /www/storage/logs
